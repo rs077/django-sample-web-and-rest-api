9c50b0db9940746d2ec1d0bbdd30d316e11f0610
rod <rsoria@migrante.com>
2024-11-06 15:02:52 +0100

    add html page for graph

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, account
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("account/", account, name='account'),
 ]
