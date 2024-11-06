9f5d1d256e2456700b809e24776eed7d37cb4ebb
rod <rsoria@migrante.com>
2024-11-06 20:32:10 +0100

    use data from json file for graphic

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,19 @@
+import json
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        with open('sample_data/data.js') as file:
+            data = json.load(file)
+        context['data'] = data
+        return context
